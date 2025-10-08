e6aefd3ab3a038025e3db0c3f8f91b7ca7007c02
lisabulka10 <es.balykova@gmail.com>
2025-10-08 17:31:42 +0300

    message

--- hr_platform/settings.py
+++ hr_platform/settings.py
@@ -61,6 +61,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
 }
 
 DATABASES = {
